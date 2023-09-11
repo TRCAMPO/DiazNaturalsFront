76940f072e0ddbe5c3de3d51ed9ad9221b59b717
Julian <julian.ramirez04@uptc.edu.co>
2023-09-11 08:46:03 -0500

    se implementan validaciones en el modulo proveedores

--- src/app/create-supplier/create-supplier.component.ts
+++ src/app/create-supplier/create-supplier.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import {ActivatedRoute, Router} from "@angular/router";
-import {NgForm} from "@angular/forms";
+import { Router} from "@angular/router";
 import {ToastrService} from "ngx-toastr";
 import {SupplierModel} from "./supplier.model";
-import {switchMap} from "rxjs";
 
 @Component({
   selector: 'app-create-supplier',
@@ -13,24 +11,71 @@ import {switchMap} from "rxjs";
 })
 export class CreateSupplierComponent {
 
-
   constructor(public authService: AuthService, private route: Router, private toast: ToastrService) {
     this.resetForm();
   }
 
   onSubmit() {
-    this.authService.postSupplier(this.authService.formDataSupplier).subscribe(
-      (response: any) => {
-        this.toast.success('Se ha creado el proveedor con exito', 'Creación de Proveedor');
-        this.resetForm();
-      },
-      (error) => {
-        this.toast.error('Falló la creación del proveedor', 'Creación de Proveedor');
-      }
-    );
+    if(!this.checkSupplierFields(this.authService.formDataSupplier)){
+      this.toast.info("Por favor llene todos los campos","Formulario Incompleto");
+    } else if(this.isValidPhone(this.authService.formDataSupplier.phoneSupplier)){
+      this.toast.info("Por favor coloque un número celular válido","Formato Incorrecto");
+    } else if(this.isValidEmail(this.authService.formDataSupplier.emailSupplier)){
+      this.toast.info("Por favor coloque un correo válido","Formato Incorrecto Correo");
+    } else{
+      this.authService.postSupplier(this.authService.formDataSupplier).subscribe(
+        (response: any) => {
+          this.toast.success('Se ha creado el proveedor con exito', 'Creación de Proveedor');
+          this.resetForm();
+        },
+        (error) => {
+          if (error.status == 409) {
+            this.toast.error(error.error, 'Creacion de Proveedor');
+          } else {
+            this.toast.error('Falló la creación del proveedor', 'Creación de Proveedor');
+          }
+        }
+      );
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    // Expresión regular para validar el formato de correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    // Comprueba si el correo electrónico coincide con la expresión regular
+    return emailRegex.test(email);
+  }
+
+  limitDigits(event: any) {
+    const maxLength = 10;
+    const inputValue = event.target.value;
+    if (inputValue.length > maxLength) {
+      event.target.value = inputValue.slice(0, maxLength);
+      this.authService.formDataUserClient.phoneClient = event.target.value;
+    }
+  }
+
+  isValidPhone(phone: string): boolean {
+    return phone.length === 10;
   }
 
   resetForm() {
     this.authService.formDataSupplier = new SupplierModel();
+  }
+
+  private checkSupplierFields(formDataProduct: SupplierModel) {
+    if(formDataProduct.addressSupplier !== "" &&
+      formDataProduct.addressSupplier !== null &&
+      formDataProduct.emailSupplier !== "" &&
+      formDataProduct.emailSupplier !== null &&
+      formDataProduct.phoneSupplier !== "" &&
+      formDataProduct.phoneSupplier !== null &&
+      formDataProduct.nameSupplier !== "" &&
+      formDataProduct.nameSupplier !== null &&
+      formDataProduct.nitSupplier !== "" &&
+      formDataProduct.nitSupplier !== null ){
+      return true;
+    }
+    return false;
   }
 }
--- src/app/edit-supplier/edit-supplier.component.ts
+++ src/app/edit-supplier/edit-supplier.component.ts
@@ -3,7 +3,6 @@ import { AuthService } from '../auth.service';
 import { Router} from "@angular/router";
 import {DataService} from "../shared/data.service";
 import {ToastrService} from "ngx-toastr";
-import {DomSanitizer} from '@angular/platform-browser';
 import {MatDialog} from "@angular/material/dialog";
 import {SupplierModel} from "../create-supplier/supplier.model";
 import {ConfirmDialogEditSupplierComponent} from "../confirm-dialog-edit-supplier/confirm-dialog-edit-supplier.component";
@@ -16,26 +15,70 @@ import {ConfirmDialogEditSupplierComponent} from "../confirm-dialog-edit-supplie
 })
 export class EditSupplierComponent {
 
-  constructor(public dialog: MatDialog, public authService: AuthService, public sanitizer: DomSanitizer, private route: Router, private dataService : DataService, private toast: ToastrService) {
+  constructor(public dialog: MatDialog, public authService: AuthService, private route: Router, private dataService : DataService, private toast: ToastrService) {
     this.authService.formDataSupplier = new SupplierModel();
     this.authService.formDataSearchSupplier.search = "";
   }
 
   onSubmit() {
-    this.authService.formDataSupplier.phoneSupplier = this.authService.formDataSupplier.phoneSupplier + "";
-    this.authService.putSupplier(this.authService.formDataSupplier).subscribe(
-      response => {
-        this.toast.success("Proveedor modificado correctamente", "Proveedor Modificado");
-        this. resetForm();
-      },
-      error => {
-        if (error.status == 409){
-          this.toast.error(error.error, 'Modificación de Proveedor');
-        }else {
-          this.toast.error("Surgió un problema en la modificación", "Proveedor no Modificado");
+    if(!this.checkSupplierFields(this.authService.formDataSupplier)){
+      this.toast.info("Por favor llene todos los campos","Formulario Incompleto");
+    } else if(this.isValidPhone(this.authService.formDataSupplier.phoneSupplier)){
+      this.toast.info("Por favor coloque un número celular válido","Formato Incorrecto");
+    } else if(this.isValidEmail(this.authService.formDataSupplier.emailSupplier)){
+      this.toast.info("Por favor coloque un correo válido","Formato Incorrecto Correo");
+    } else {
+      this.authService.formDataSupplier.phoneSupplier = this.authService.formDataSupplier.phoneSupplier + "";
+      this.authService.putSupplier(this.authService.formDataSupplier).subscribe(
+        () => {
+          this.toast.success("Proveedor modificado correctamente", "Proveedor Modificado");
+          this.resetForm();
+        },
+        error => {
+          if (error.status == 409) {
+            this.toast.error(error.error, 'Modificación de Proveedor');
+          } else {
+            this.toast.error("Surgió un problema en la modificación", "Proveedor no Modificado");
+          }
         }
-      }
-    );
+      );
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    // Expresión regular para validar el formato de correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    // Comprueba si el correo electrónico coincide con la expresión regular
+    return emailRegex.test(email);
+  }
+
+  limitDigits(event: any) {
+    const maxLength = 10;
+    const inputValue = event.target.value;
+    if (inputValue.length > maxLength) {
+      event.target.value = inputValue.slice(0, maxLength);
+      this.authService.formDataUserClient.phoneClient = event.target.value;
+    }
+  }
+
+  isValidPhone(phone: string): boolean {
+    return phone.length === 10;
+  }
+
+  private checkSupplierFields(formDataProduct: SupplierModel) {
+    if(formDataProduct.addressSupplier !== "" &&
+      formDataProduct.addressSupplier !== null &&
+      formDataProduct.emailSupplier !== "" &&
+      formDataProduct.emailSupplier !== null &&
+      formDataProduct.phoneSupplier !== "" &&
+      formDataProduct.phoneSupplier !== null &&
+      formDataProduct.nameSupplier !== "" &&
+      formDataProduct.nameSupplier !== null &&
+      formDataProduct.nitSupplier !== "" &&
+      formDataProduct.nitSupplier !== null ){
+      return true;
+    }
+    return false;
   }
 
   openConfirmationDialog(): void {
@@ -68,7 +111,7 @@ export class EditSupplierComponent {
           // @ts-ignore
           this.toast.success("Se encontró el proveedor", "Proveedor Encontrado")
         },
-        (error) => {
+        () => {
           this.toast.error("No se pudo encontrar el proveedor", "Error en la Búsqueda");
         }
       );
