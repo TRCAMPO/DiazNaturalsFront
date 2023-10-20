6d2a4ffe1656134f1b5a5db1a1ba766bb5f3950a
Julian <julian.ramirez04@uptc.edu.co>
2023-10-20 02:04:31 -0500

    Se implementa listar proveedores en su totalidad de funcionalidades

--- src/app/list-suppliers/list-suppliers.component.ts
+++ src/app/list-suppliers/list-suppliers.component.ts
@@ -6,6 +6,12 @@ import {
 import {AuthService} from "../auth.service";
 import {SupplierModel} from "../create-supplier/supplier.model";
 import {ToastrService} from "ngx-toastr";
+import {Router} from "@angular/router";
+import {SharedDataService} from "./shareDataService";
+import {
+  ConfirmDialogDeleteSupplierComponent
+} from "../confirm-dialog-delete-supplier/confirm-dialog-delete-supplier.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-list-suppliers',
@@ -18,7 +24,7 @@ export class ListSuppliersComponent implements OnInit{
   suppliersOrigin: SupplierModel[] = [];
   currentPage = 1;
   elementeForPage = 5;
-  constructor(private toast: ToastrService, public authService: AuthService)  {
+  constructor(public dialog: MatDialog, private toast: ToastrService, public authService: AuthService, public router: Router, private sharedDataService: SharedDataService)  {
 
   }
   ngOnInit() {
@@ -34,12 +40,23 @@ export class ListSuppliersComponent implements OnInit{
     );
   }
 
-  deleteproduct(){
-
+  deleteproduct(nameSupplier: string){
+    this.authService.getSupplierByName(nameSupplier).subscribe(
+      (data) => {
+        this.authService.formDataSupplier = data;
+      },
+      (error) => {
+      }
+    );
+    this.openConfirmationDialog();
   }
 
-  editproduct(){
-
+  editproduct(nameSupplier: string){
+    const productData = nameSupplier;
+    // Utiliza el servicio para establecer los datos
+    this.sharedDataService.setProductData(productData);
+    // Navega a la pantalla de editar producto
+    this.router.navigate(['/editSupplier']);
   }
 
   searchEditSupplier() {
@@ -62,6 +79,46 @@ export class ListSuppliersComponent implements OnInit{
     }
   }
 
+  onSubmit() {
+    this.authService.formDataDeleteSupplier.nitSupplier = this.authService.formDataSupplier.nitSupplier;
+    this.authService.formDataDeleteSupplier.isActive = false;
+    this.authService.formDataSupplier.phoneSupplier = this.authService.formDataSupplier.phoneSupplier+"";
+    this.authService.patchSupplier(this.authService.formDataDeleteSupplier).subscribe(
+      response => {
+        this.toast.success("Proveedor eliminado correctamente", "Proveedor Eliminado");
+        this.resetForm();
+      },
+      error => {
+        this.toast.error("Surgio un problema en la eliminación", "Usuario no Eliminado");
+      }
+    );
+  }
+
+  openConfirmationDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmDialogDeleteSupplierComponent, {
+      panelClass: 'custom-dialog-overlay', // Clase CSS personalizada
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.onSubmit();
+      } else {
+      }
+    });
+  }
+
+  resetForm() {
+    this.authService.formDataSupplier = new SupplierModel();
+    this.authService.getSuppliers().subscribe(
+      (data) => {
+        this.suppliers = data;
+        this.suppliersOrigin = data;
+        // @ts-ignore
+      },
+      () => {
+        this.toast.error("No se pudieron encontrar proveedores", "Error en la Búsqueda");
+      }
+    );
+  }
 
 }
 
